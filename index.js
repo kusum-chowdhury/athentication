3dee4f8b9d9db8632b85bfaebfd817a3250ff729
kusum chowdhury <kusumchowdhury5832@gmail.com>
2023-09-08 08:21:51 -0700

    Add 'public' directory and create 'signup.html' and 'login.html' pages for backend authentication

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const app = express();
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+app.use(express.static("public"));
 
-const PORT = 3000;
+const PORT = 8000;
 app.listen(PORT, ()=> {
     console.log("app is running at port", PORT);
 })
